750f6a36bdc9f53b3db5cf1b663047e7196b6ee7
tong92 <tong92power@gmail.com>
2018-12-05 01:57:18 +0900

    Adds example problem
    - adds 2.1.1, 2.1.2
    - add 2.2.1
    - lang: racket

---
+++ ch2/ex2-1-1.rkt
@@ -0,0 +1,16 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-advanced-reader.ss" "lang")((modname ex2-1-1) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #t #t none #f () #f)))
+(= (expt 2 2) 4)
+
+(= (expt 3 2) 9)
+
+(= (expt 2 3) 8)
+
+(sin 1)
+
+(sin 1.5)
+
+(> 1 0)
+
+(> 1 3)
---
+++ ch2/ex2-1-2.rkt
@@ -0,0 +1,14 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-advanced-reader.ss" "lang")((modname ex2-1-2) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #t #t none #f () #f)))
+(sqrt 4)
+
+(sqrt 2)
+
+(sqrt -1)
+
+(tan 1)
+
+(tan 1.5)
+
+(tan 0)
