c872de96c2eb8e45c44eda2856b5ed0f11213cc4
tong92 <tong92power@gmail.com>
2018-12-11 00:59:39 +0900

    Solve ch2 example 4
    - add 2.4.2

---
+++ ch2/ex2-4-2.rkt
@@ -0,0 +1,14 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-advanced-reader.ss" "lang")((modname ex2-4-2) (read-case-sensitive #t) (teachpacks ((lib "convert.rkt" "teachpack" "htdp"))) (htdp-settings #(#t constructor repeating-decimal #t #t none #f ((lib "convert.rkt" "teachpack" "htdp")) #f)))
+(define (f x)
+  (+ x 10)
+)
+
+(define (g x)
+  (+ x 10)
+)
+
+(define (h x)
+  (+ x 10)
+)
