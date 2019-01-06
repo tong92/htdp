3b3fb4ae0e2730fb164e0463916cf512d9da7a84
tong92 <tong92power@gmail.com>
2019-01-07 02:17:24 +0900

    Solve ch6 example 2
    - adds 6-2-1 to 6-2-5

---
+++ ch6/ex6-2-1.rkt
@@ -0,0 +1,8 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-advanced-reader.ss" "lang")((modname ex6-2-1) (read-case-sensitive #t) (teachpacks ((lib "convert.rkt" "teachpack" "htdp") (lib "guess.rkt" "teachpack" "htdp") (lib "guess-gui.rkt" "teachpack" "htdp") (lib "master.rkt" "teachpack" "htdp") (lib "draw.rkt" "teachpack" "htdp"))) (htdp-settings #(#t constructor repeating-decimal #t #t none #f ((lib "convert.rkt" "teachpack" "htdp") (lib "guess.rkt" "teachpack" "htdp") (lib "guess-gui.rkt" "teachpack" "htdp") (lib "master.rkt" "teachpack" "htdp") (lib "draw.rkt" "teachpack" "htdp")) #f)))
+(start 300 300)
+(draw-solid-line (make-posn 1 1) (make-posn 5 5) 'red)
+(draw-solid-rect (make-posn 20 10) 50 200 'blue)
+(draw-circle (make-posn 200 10) 50 'red)
+;(draw-solid-disk (make-posn 200 10) 50 'green)
---
+++ ch6/ex6-2-5.rkt
@@ -0,0 +1,89 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-advanced-reader.ss" "lang")((modname ex6-2-5) (read-case-sensitive #t) (teachpacks ((lib "convert.rkt" "teachpack" "htdp") (lib "guess.rkt" "teachpack" "htdp") (lib "guess-gui.rkt" "teachpack" "htdp") (lib "master.rkt" "teachpack" "htdp") (lib "draw.rkt" "teachpack" "htdp"))) (htdp-settings #(#t constructor repeating-decimal #t #t none #f ((lib "convert.rkt" "teachpack" "htdp") (lib "guess.rkt" "teachpack" "htdp") (lib "guess-gui.rkt" "teachpack" "htdp") (lib "master.rkt" "teachpack" "htdp") (lib "draw.rkt" "teachpack" "htdp")) #f)))
+;; Constants
+(define WIDTH 50)
+(define HEIGHT 160)
+(define BULB-RADIUS 20)
+(define BULB-DISTANCE 10)
+
+;; position
+(define X-BULBS (quotient WIDTH 2))
+(define Y-RED (+ BULB-DISTANCE BULB-RADIUS))
+(define Y-YELLOW (+ Y-RED BULB-DISTANCE (* 2 BULB-RADIUS)))
+(define Y-GREEN (+ Y-YELLOW BULB-DISTANCE (* 2 BULB-RADIUS)))
+
+;; Lightin RED Only
+(start WIDTH HEIGHT)
+(draw-solid-disk (make-posn X-BULBS Y-RED) BULB-RADIUS 'red)
+(draw-circle (make-posn X-BULBS Y-YELLOW) BULB-RADIUS 'yellow)
+(draw-circle (make-posn X-BULBS Y-GREEN) BULB-RADIUS 'green)
+
+;; clear-bulb Color -> bool
+(define (clear-bulb color)
+  (cond
+    [(symbol=? color 'red)
+     (and
+      (clear-solid-disk (make-posn X-BULBS Y-RED) BULB-RADIUS 'red)
+      (draw-circle (make-posn X-BULBS Y-RED) BULB-RADIUS 'red)
+      )
+     ]
+    [(symbol=? color 'yellow)
+     (and
+      (clear-solid-disk (make-posn X-BULBS Y-YELLOW) BULB-RADIUS 'yellow)
+      (draw-circle (make-posn X-BULBS Y-YELLOW) BULB-RADIUS 'yellow)
+      )
+     ]
+    [(symbol=? color 'green)
+     (and
+      (clear-solid-disk (make-posn X-BULBS Y-GREEN) BULB-RADIUS 'green)
+      (draw-circle (make-posn X-BULBS Y-GREEN) BULB-RADIUS 'green)
+      )
+     ] 
+  )
+)
+
+;; draw-bulb
+(define (draw-bulb color)
+  (cond
+    [(symbol=? color 'red)
+     (and
+      (clear-circle (make-posn X-BULBS Y-RED) BULB-RADIUS 'red)
+      (draw-solid-disk (make-posn X-BULBS Y-RED) BULB-RADIUS 'red)
+      )
+     ]
+    [(symbol=? color 'yellow)
+     (and
+      (clear-circle (make-posn X-BULBS Y-YELLOW) BULB-RADIUS 'yellow)
+      (draw-solid-disk (make-posn X-BULBS Y-YELLOW) BULB-RADIUS 'yellow)
+      )
+     ]
+    [(symbol=? color 'green)
+     (and
+      (clear-circle (make-posn X-BULBS Y-GREEN) BULB-RADIUS 'green)
+      (draw-solid-disk (make-posn X-BULBS Y-GREEN) BULB-RADIUS 'green)
+      )
+     ]
+  )
+)
+
+(define (switch prev next)
+  (and
+   (clear-bulb prev)
+   (draw-bulb next)
+   )
+)
+
+;; next : symbol -> symbol
+(define (next color)
+  (cond
+    [(and (symbol=? color 'red) (switch 'red 'green)) 'green]
+    [(and (symbol=? color 'yellow) (switch 'yellow 'red)) 'red]
+    [(and (symbol=? color 'green) (switch 'green 'yellow)) 'yellow]
+  )
+)
+
+(define next-color1 (next 'red))
+(define next-color2 (next next-color1))
+(define next-color3 (next next-color2))
+(define next-color4 (next next-color3))
